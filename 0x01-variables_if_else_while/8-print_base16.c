e707529055401938c583748d6b32d9000eba4d8f
AdeMotoro <93820103+AdewaleMotoro@users.noreply.github.com>
2022-06-05 19:34:16 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Prints all the numbers of base 16 in lowercase
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -15,7 +15,7 @@ int main(void)
 
 	for (letter = 'a'; letter <= 'f'; letter++)
 		putchar(letter);
-putchar('\n');
 	
+putchar('\n');
 	return (0);
 }
