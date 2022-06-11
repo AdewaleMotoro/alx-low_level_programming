0885e8c8f7e3951768fd5efb1cfa4e47f5ddc51b
AdeMotoro <93820103+AdewaleMotoro@users.noreply.github.com>
2022-06-11 07:24:15 +0100

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,21 +11,21 @@ void print_triangle(int size)
 
 	if (size <= 0)
 	{
-		_putchar('\n');
+	_putchar('\n');
 	}
 	else
 	{
-		for (ro = 0; ro <= (size - 1); ro++)
-		{
-			for (sp = 0; sp < (size - 1) - ro; sp++)
-			{
-				_putchar(' ');
-			}
-			for (tr = 0; tr <= ro; tr++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
+	for (ro = 0; ro <= (size - 1); ro++)
+	{
+	for (sp = 0; sp < (size - 1) - ro; sp++)
+	{
+	_putchar(' ');
+	}
+	for (tr = 0; tr <= ro; tr++)
+	{
+	_putchar('#');
+	}
+	_putchar('\n');
+	}
 	}
 }
