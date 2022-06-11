83e6467fe5d682a934eaebf91e73568ceee77302
AdeMotoro <93820103+AdewaleMotoro@users.noreply.github.com>
2022-06-11 07:13:43 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,14 +11,14 @@ void print_line(int n)
 
 	if (n <= 0)
 	{
-		_putchar('\n');
+	_putchar('\n');
 	}
 	else
 	{
-		for (co = 1; co <= n; co++)
-		{
-			_putchar('_');
-		}
-		_putchar('\n');
+	for (co = 1; co <= n; co++)
+	{
+	_putchar('_');
+	}
+	_putchar('\n');
 	}
 }
