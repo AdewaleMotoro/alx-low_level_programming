8b26d2edf7f607ac1663ff887c54efe3f4ca5b2b
AdeMotoro <93820103+AdewaleMotoro@users.noreply.github.com>
2022-06-13 19:56:29 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,7 @@
-int _putchar(char c);
+#ifndef main_h
+#define main_h
+
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -11,6 +14,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-101-natural.c
-102-fibonacci.c
-103-fibonacci.c
+
+#endif
