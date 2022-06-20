baf21cdf1bea68084687c2cad1073793b61be142
AdeMotoro <93820103+AdewaleMotoro@users.noreply.github.com>
2022-06-20 21:34:49 +0100

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,12 +11,12 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int index;
-	char *destination = dest;
-	char *source = src;
+unsigned int index;
+char *destination = dest;
+char *source = src;
 
-	for (index = 0; index < n; index++)
-		destination[index] = source[index];
+for (index = 0; index < n; index++)
+destination[index] = source[index];
 
-	return (dest);
+return (dest);
 }
